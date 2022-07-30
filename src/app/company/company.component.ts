43caf84aa9e4779e8a83a7dc82da0c0882c0dae6
Ean Milligan (Bastion) <ean.milligan@gmail.com>
2022-07-29 21:30:11 -0400

    add hover hint from when hovering skills

--- src/app/company/company.component.ts
+++ src/app/company/company.component.ts
@@ -1,5 +1,6 @@
 import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, Input, OnInit } from '@angular/core';
+import { ExperienceService } from '../experience.service';
 import { Company } from '../structs';
 
 @Component({
@@ -24,12 +25,20 @@ import { Company } from '../structs';
 export class CompanyComponent implements OnInit {
   @Input() company: Company = <Company>{};
   @Input() projectsState: string = 'hidden';
+  companySkillIds: string[] = [];
+  shouldHover: boolean = false
+
+  constructor(private experienceService: ExperienceService) {
+    this.experienceService.hoveredSkillIdChange.subscribe(value => this.shouldHover = this.companySkillIds.includes(value));
+  }
 
   toggleExpanded(): void {
     this.projectsState = this.projectsState === 'hidden' ? 'shown' : 'hidden';
   }
 
   ngOnInit(): void {
+    for (const project of this.company.projects)
+    this.companySkillIds.push(...project.skills);
     if (this.company.endDate.toLowerCase() === 'current') {
       this.projectsState = 'shown';
     }
